dc7e4ddd32a1160f708681bc095ec93d43cfee37
Richard <rcarrion3406@gmail.com>
2022-06-19 00:45:58 -0500

    Routing added

--- desktop_client/src/App.js
+++ desktop_client/src/App.js
@@ -1,32 +1,17 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import Main from "./views/main/Main";
+import React from "react";
+import { HashRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./views/home/Home";
 import Login from "./views/login/Login";
-import Sidebar from "./components/sidebar/Sidebar";
-import DialogContainer from "./components/dialog/dialogContainer";
-import BranchForm from "./components/forms/branch/branchForm";
-import { Button } from "@mui/material";
-import Maps from "./components/maps/Maps";
 
 function App() {
-  const [open, setOpen] = useState(false);
-
-  const handleClickToOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClickToClose = () => {
-    setOpen(false);
-  };
-
   return (
-    <>
-      <Button onClick={handleClickToOpen}>Abrir</Button>
-      <DialogContainer title="Dialogo" open={open} onClose={handleClickToClose}>
-        <BranchForm onSave={handleClickToClose}></BranchForm>
-      </DialogContainer>
-
-    </>
+    <HashRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/home" element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
