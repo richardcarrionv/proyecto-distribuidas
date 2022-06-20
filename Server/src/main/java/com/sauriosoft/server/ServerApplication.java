5344f8f3a2e262f513f56200945ea87723b3ad73
TigselemaAlex <84351430+TigselemaAlex@users.noreply.github.com>
2022-06-20 12:53:27 -0500

    CORS ALL ORIGIN CONFIGURATION

--- Server/src/main/java/com/sauriosoft/server/ServerApplication.java
+++ Server/src/main/java/com/sauriosoft/server/ServerApplication.java
@@ -18,7 +18,10 @@ public class ServerApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+                registry.addMapping("/**").allowedOrigins("http//localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                                "Access-Control-Request-Headers");
             }
         };
     }
