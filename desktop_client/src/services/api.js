4c293e6a3f7a93902b30d314479694c0f408ed71
Richard <rcarrion3406@gmail.com>
2022-07-07 11:32:19 -0500

    Branch Crud and ignore

--- desktop_client/src/services/api.js
+++ desktop_client/src/services/api.js
@@ -1,4 +1,6 @@
 import axios from "axios"; 
+const heroku = "https://onealarm.herokuapp.com/api";
+const local = "http://localhost:8081/api";
 export default axios.create({ 
-  baseURL: "https://onealarm.herokuapp.com/api"
+  baseURL: local
 });
