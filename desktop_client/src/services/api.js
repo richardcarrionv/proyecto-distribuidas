04d12515fd369319185f14eb5611d35dba81ef2d
Richard <rcarrion3406@gmail.com>
2022-07-07 11:34:32 -0500

    Prod variables

--- desktop_client/src/services/api.js
+++ desktop_client/src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 const heroku = "https://onealarm.herokuapp.com/api";
 const local = "http://localhost:8081/api";
 export default axios.create({ 
-  baseURL: local
+  baseURL: heroku
 });
