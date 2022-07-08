3960de243f20d7e61dd9d176a4fe0611a61c5e66
Richard <rcarrion3406@gmail.com>
2022-07-07 22:47:34 -0500

    Refactor controllers

--- Server/src/main/java/com/sauriosoft/server/controllers/UserController.java
+++ Server/src/main/java/com/sauriosoft/server/controllers/UserController.java
@@ -4,105 +4,62 @@ import com.sauriosoft.server.models.dtos.UserDTO;
 import com.sauriosoft.server.models.entities.User;
 import com.sauriosoft.server.models.exceptions.UserException;
 import com.sauriosoft.server.models.services.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/users/")
+@RequestMapping(value = "/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @Operation(summary = "Get all users", responses = {
-            @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json"), useReturnTypeSchema = true),
-            @ApiResponse(description = "Empty", responseCode = "204"),
-            @ApiResponse(description = "Server error", responseCode = "503")
-    })
     @GetMapping()
-    public ResponseEntity<?> getAll() {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<List<UserDTO>> getAll() {
         List<User> userList = userService.getAll();
         if (userList.isEmpty()) {
-            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        } else {
-            List<UserDTO> userDTOS = userList.stream().map(UserDTO::from).collect(Collectors.toList());
-            response.put("response", userDTOS);
-            return new ResponseEntity<>(response, HttpStatus.OK);
+            return ResponseEntity.noContent().build();
         }
+        List<UserDTO> userDTOS = userList
+                .stream()
+                .map(UserDTO::from)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(userDTOS);
 
     }
 
-    @Operation(summary = "Get one User by Id", responses = {
-            @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json"), useReturnTypeSchema = true),
-            @ApiResponse(description = "Server Error", responseCode = "503"),
-            @ApiResponse(description = "User not found", responseCode = "500")
-    })
-    @GetMapping("{id}")
-    public ResponseEntity<?> getById(@PathVariable(name = "id") final Long idUser) {
-        Map<String, Object> response = new HashMap<>();
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getById(@PathVariable(name = "id") final Long idUser) {
         User user = userService.getById(idUser);
-        response.put("response", UserDTO.from(user));
-        return new ResponseEntity<>(response, HttpStatus.OK);
-
+        return ResponseEntity.ok(UserDTO.from(user));
     }
 
-    @Operation(summary = "Save one User", responses = {
-            @ApiResponse(description = "Successfully created", responseCode = "201", content = @Content(mediaType = "application/json"), useReturnTypeSchema = true),
-            @ApiResponse(description = "Server error", responseCode = "503"),
-            @ApiResponse(description = "Body with Id", responseCode = "500")
-    })
     @PostMapping
-    public ResponseEntity<?> addUser(@RequestBody final UserDTO userDTO) {
-        Map<String, Object> response = new HashMap<>();
-        if (Objects.isNull(userDTO.getId())) {
-            User user = User.from(userDTO);
-            user = userService.create(user);
-            response.put("response", UserDTO.from(user));
-            return new ResponseEntity<>(response, HttpStatus.CREATED);
+    public ResponseEntity<UserDTO> addUser(@RequestBody final UserDTO userDTO) {
+        if (!Objects.isNull(userDTO.getId())) {
+            throw new UserException("Ya existe un usuario con el id: ".concat(userDTO.getId().toString()));
         }
-        throw new UserException("Ya existe un usuario con el id: ".concat(userDTO.getId().toString()));
+        User user = User.from(userDTO);
+        user = userService.create(user);
+        return ResponseEntity.ok(UserDTO.from(user));
     }
 
-    @Operation(summary = "Update one User", responses = {
-            @ApiResponse(description = "Successfully updated", responseCode = "201", content = @Content(mediaType = "application/json"), useReturnTypeSchema = true),
-            @ApiResponse(description = "Server error", responseCode = "503"),
-            @ApiResponse(description = "User not found", responseCode = "500")
-    })
-    @PutMapping("{id}")
-    public ResponseEntity<?> updateUser(@PathVariable(name = "id") final Long idUser,
-                                        @RequestBody final UserDTO userDTO) {
-        Map<String, Object> response = new HashMap<>();
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable(name = "id") final Long idUser,
+                                              @RequestBody final UserDTO userDTO) {
         User userToUpdate = User.from(userDTO);
         UserDTO userDTOFromUpdate = UserDTO.from(userService.update(userToUpdate, idUser));
-        response.put("response", userDTOFromUpdate);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
-
+        return ResponseEntity.ok(userDTOFromUpdate);
     }
 
-    @Operation(summary = "Delete one Contact", responses = {
-            @ApiResponse(description = "Successfully delete", responseCode = "200", content = @Content(mediaType = "application/json"), useReturnTypeSchema = true),
-            @ApiResponse(description = "Server error", responseCode = "503"),
-            @ApiResponse(description = "User not found", responseCode = "500")
-    })
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable(name = "id") final Long idUser) {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<String> deleteUser(@PathVariable(name = "id") final Long idUser) {
         userService.delete(idUser);
-        response.put("response", "Usuario eliminado");
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ResponseEntity.ok("Registro eliminado correctamente");
     }
-
-
 }
