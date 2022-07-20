92a5f8b6ba3532fea007aa497e6ae7a4adfacf07
Richard <rcarrion3406@gmail.com>
2022-07-19 23:25:26 -0500

    push notification from server

--- Server/src/main/java/com/sauriosoft/server/controllers/AlarmController.java
+++ Server/src/main/java/com/sauriosoft/server/controllers/AlarmController.java
@@ -7,10 +7,17 @@ import com.sauriosoft.server.models.entities.Igniter;
 import com.sauriosoft.server.models.exceptions.AlarmException;
 import com.sauriosoft.server.models.services.AlarmService;
 import com.sauriosoft.server.models.services.IgniterService;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -54,8 +61,47 @@ public class AlarmController {
         }
         Igniter igniter = igniterService.getById(alarmDTO.getIgniterId());
         Alarm alarm = Alarm.from(alarmDTO, igniter);
+        try {
+            sendNotification();
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
         alarm = alarmService.create(alarm);
         return ResponseEntity.ok(AlarmDTO.from(alarm));
     }
 
+      private String sendNotification() throws IOException, JSONException {
+
+            JSONObject body = new JSONObject();
+            body.put("to", "/topics/news");
+          JSONObject data = new JSONObject();
+          data.put("message", "Hola" );
+          data.put("coords", "0123,12312" );
+          data.put("quien", "Usuario 3" );
+          JSONObject notification = new JSONObject();
+          notification.put("title", "Testeo Spring boot");
+          notification.put("body", "Teste Spring boot body");
+          notification.put("badge", 1);
+          notification.put("sound", "ping.aiff");
+
+          body.put("data", data);
+          body.put("notification", notification);
+
+
+         OkHttpClient client = new OkHttpClient();
+
+         String url = "https://api.pushy.me/push?api_key=36f1c684545748d9db057ee8a90c666d479db7bba1285bc8fdbf2f1824c2af93";
+
+        MediaType mediaType = MediaType.parse("application/json");
+        okhttp3.RequestBody bodyRequest = okhttp3.RequestBody.create(body.toString(), mediaType);
+        System.out.println(bodyRequest);
+        Request request = new Request.Builder()
+          .url(url)
+          .post(bodyRequest)
+          .addHeader("Content-Type", "application/json")
+          .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().toString();
+    }
 }
