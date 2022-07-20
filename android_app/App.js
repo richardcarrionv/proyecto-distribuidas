ca4ac639901682e192559f3943b00fe5f029eef4
Richard <rcarrion3406@gmail.com>
2022-07-19 22:38:51 -0500

    Pushy mobile working

--- android_app/App.js
+++ android_app/App.js
@@ -1,20 +1,79 @@
-import {extendTheme, NativeBaseProvider} from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/views/main';
+import { extendTheme, NativeBaseProvider } from "native-base";
+import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/views/main";
+import Pushy from "pushy-react-native";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    Pushy.listen();
+    // Register the user for push notifications
+    Pushy.register()
+      .then(async (deviceToken) => {
+        // Display an alert with device token
+        alert("Pushy device token: " + deviceToken);
+
+        // Send the token to your backend server via an HTTP GET request
+        //await fetch('https://your.api.hostname/register/device?token=' + deviceToken);
+
+        // Succeeded, optionally do something to alert the user
+      })
+      .catch((err) => {
+        // Handle registration errors
+        console.error(err);
+      });
+    Pushy.toggleInAppBanner(true);
+
+    Pushy.setNotificationListener(async (data) => {
+      // Print notification payload data
+      console.log("Received notification: " + JSON.stringify(data));
+
+      // Notification title
+      let notificationTitle = "MyApp";
+
+      // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
+      let notificationText = data.message || "Test notification";
+
+      // Android: Displays a system notification
+      // iOS: Displays an alert dialog
+      Pushy.notify(notificationTitle, notificationText, data);
+
+      // Clear iOS badge count
+      Pushy.setBadge(0);
+    });
+    Pushy.setNotificationClickListener(async (data) => {
+      // Display basic alert
+      alert("Notification click: " + data.message);
+
+      // Navigate the user to another page or
+      // execute other logic on notification click
+    });
+    Pushy.isRegistered().then((isRegistered) => {
+      if (isRegistered) {
+        // Subscribe the user to a topic
+        Pushy.subscribe("news")
+          .then(() => {
+            // Subscribe successful
+            alert("Subscribed to topic successfully");
+          })
+          .catch((err) => {
+            // Handle errors
+            console.error(err);
+          });
+      }
+    });
+  }, []);
 
   const theme = extendTheme({
-    components: { 
-      Button: { 
-        baseStyle: { 
-        }, 
-        defaultProps: { 
-          colorScheme: "red"
-        }
-      }
-    }
-  })
+    components: {
+      Button: {
+        baseStyle: {},
+        defaultProps: {
+          colorScheme: "red",
+        },
+      },
+    },
+  });
   return (
     <NativeBaseProvider theme={theme}>
       <Main />
@@ -22,10 +81,9 @@ export default function App() {
   );
 }
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
-
